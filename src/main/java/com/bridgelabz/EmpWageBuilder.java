3be6a176eaf265f8b49a0287c2ac0caf9812c574
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-19 18:32:43 +0530

    ADDED:To use the switch case statement employee wage computation

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -7,32 +7,33 @@ public class EmpWageBuilder {
 
     public static void main(String[] args) {
 
+        //object
         Random attendance = new Random();
 
         //variable
         int wage_per_hour=20;
         int hour_in_day=8;
         int half_time=4;
-
-        //constant
-        int IS_PRESENT = 1;
+        int totalWage_PartTime;
+        int totalWage_ofDaily;
 
         //size
         int max=3;
         int empCheck=attendance.nextInt(max);
-        System.out.println(IS_PRESENT);
-        if(empCheck == IS_PRESENT) {
-            int totalWage_ofDaily = wage_per_hour * hour_in_day;
-            System.out.println("total Wage for Day : " + totalWage_ofDaily);
-            System.out.println("employee is present");
-        }else if (empCheck == 2)
+        switch (empCheck)
         {
-            int totalWage_PartTime;
-            totalWage_PartTime=wage_per_hour*half_time;
-            System.out.println("PartTime Wage : " + totalWage_PartTime);
-        }else {
-            System.out.println("employee is absent");
-        }
+            case 1:
+                totalWage_ofDaily=wage_per_hour*hour_in_day;
+                System.out.println("total Wage for Day : " + totalWage_ofDaily);
+                break;
+            case 2:
+                totalWage_PartTime=wage_per_hour*half_time;
+                System.out.println("PartTime Wage : " + totalWage_PartTime);
+                break;
+            default:
+                System.out.println("Employee is absent");
+                break;
+            }
 
 
         }
