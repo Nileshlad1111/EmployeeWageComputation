cc97aae1cc5ce397b49c66f6c9b71aec866bebf6
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-19 19:27:01 +0530

    ADDED:To calculate wages for a month

--- src/main/java/com/bridgelabz/EmpWageBuilder.java
+++ src/main/java/com/bridgelabz/EmpWageBuilder.java
@@ -11,30 +11,39 @@ public class EmpWageBuilder {
         Random attendance = new Random();
 
         //variable
-        int wage_per_hour=20;
-        int hour_in_day=8;
-        int half_time=4;
-        int totalWage_PartTime;
-        int totalWage_ofDaily;
+        int wage_per_hour = 20;
+        int hour_in_day = 8;
+        int half_time = 4;
+        int totalWage = 0;
+        int total_day = 0;
+        int total_hour = 0;
+        int total_working_day = 20;
 
         //size
-        int max=3;
-        int empCheck=attendance.nextInt(max);
-        switch (empCheck)
-        {
-            case 1:
-                totalWage_ofDaily=wage_per_hour*hour_in_day;
-                System.out.println("total Wage for Day : " + totalWage_ofDaily);
-                break;
-            case 2:
-                totalWage_PartTime=wage_per_hour*half_time;
-                System.out.println("PartTime Wage : " + totalWage_PartTime);
-                break;
-            default:
-                System.out.println("Employee is absent");
-                break;
-            }
+        int maximumSize = 3;
 
+            while (total_day < total_working_day) {
+                int empCheck = attendance.nextInt(maximumSize);
+                switch (empCheck) {
+                    case 1:
+                        totalWage = totalWage + wage_per_hour * hour_in_day;
+                        total_day = total_day + 1;
+                        System.out.println("Daily employee wage");
+                        break;
+                    case 2:
+                        totalWage = totalWage + wage_per_hour * half_time;
+                        total_hour = total_hour + 4;
+                        System.out.println("Part time");
+                        break;
+                    default:
+                        System.out.println("Employee is absent");
+                        break;
+                }
+            }
+            System.out.println("Total Wage for month : " + totalWage);
+            System.out.println("total Days : " + total_day);
+            System.out.println("total hour : " + total_hour);
 
         }
+
     }
